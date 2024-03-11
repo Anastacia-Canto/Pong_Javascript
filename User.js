83c8b18d4cb5f9e0580a9e5ac96a3b8f1c5f3c6b
Anastacia <anaanestesia@gmail.com>
2024-03-11 15:37:16 +0000

    migrating to classes approach

---
+++ User.js
@@ -0,0 +1,18 @@
+export default class User {
+
+	constructor(username) {
+		this.username = username;
+		this.score = 0;
+		this.games = [];
+	}
+
+	storeGames(id, result, opponent) {
+		let game = {
+			id: id,
+			result: result,
+			opponent: opponent
+		};
+		this.games.push(game);
+	}
+
+}
